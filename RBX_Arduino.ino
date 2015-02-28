dfb1d65cabc2eedb9da65300835a204d281ec0e1
Matthew Hennes <matthew.hennes@gmail.com>
2015-02-27 16:54:51 -0800

    Added RBX_Arduino.ino
    I'm confused, this ino file seems to have the same (basic) code as
    'Manual Winch Code'. In any case, arduino likes opening this one, and it
    has updates that Daniel made.

---
+++ RBX_Arduino.ino
@@ -0,0 +1,131 @@
+/*
+Bit 0 = fwdIn
+ Bit 1 = backIn
+ Bit 2 = fwdStopIn
+ Bit 3 = backStopIn  
+ Bit 4 = upIn
+ Bit 5 = downIn
+ Bit 6 = downStopIn
+ Bit 7 = unassigned
+ */
+int FWD_IN_OFFSET = 0;
+int BACK_IN_OFFSET = 1;
+int FWD_STOP_IN = 2;
+int BACK_STOP_IN = 3;
+int UP_IN_OFFSET = 4;
+int DOWN_IN_OFFSET = 5;
+int DOWN_STOP_IN = 6;
+
+//4 inputs 
+int fwdIn = 16;      //B2 
+int backIn = 14;     //B3 
+int fwdStopIn = 4;   //D4
+int backStopIn = 10; //B6
+int manualUp = 6     //D7  
+int manualDown = 12  //D6
+int downStopIn = 3   //D0
+//ERROR PIN
+int ERROR_PIN = 7;    //E6
+
+//2 outputs
+int fwdOut = 13;      //C7
+int backOut = 5;      //C6
+
+uint8_t prevState = 0x00;
+uint8_t currState = 0x00;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(fwdIn, INPUT);
+  pinMode(backIn, INPUT);
+  pinMode(fwdStopIn, INPUT);
+  pinMode(backStopIn, INPUT);
+  digitalWrite(fwdIn, HIGH);
+  digitalWrite(backIn, HIGH);
+  digitalWrite(fwdStopIn, HIGH);
+  digitalWrite(backStopIn, HIGH);
+
+  pinMode(ERROR_PIN,OUTPUT);
+  pinMode(fwdOut,OUTPUT);
+  pinMode(backOut,OUTPUT);
+  digitalWrite(ERROR_PIN,LOW);
+  digitalWrite(fwdOut, LOW);
+  digitalWrite(backOut, LOW);
+}
+
+uint8_t checkInputs(){
+  uint8_t tempState = 0x00;
+  tempState |= (!digitalRead(fwdIn)) << FWD_IN_OFFSET;
+  tempState |= (!digitalRead(backIn)) << BACK_IN_OFFSET;
+  tempState |= (!digitalRead(fwdStopIn)) << FWD_STOP_IN;
+  tempState |= (!digitalRead(backStopIn)) << BACK_STOP_IN;  
+  return tempState;
+}
+
+void loop() {
+  prevState = currState;
+  currState = checkInputs();
+  Serial.print("State: ");
+  Serial.println(currState);
+  delay(100);
+
+  switch (currState) {
+  case 0x00:
+    break;
+
+  case 0x01: //MOVE FWD SWITCH IS ON
+    movFwd();
+    break;
+
+  case 0x05: //MOVE FWD, BUT FWD END IS ON
+    stopCarts();
+    break;
+
+  case 0x02: //MOVE BACKWARD SWITCH IS ON
+    movRev();
+    break;
+
+  case 0x0A:  //MOVE BACKWARD, BUT BACK END IS ON
+    stopCarts();
+    break;
+
+  default: //LOL GG DONE MESSED UP SON
+      error();
+    break;
+  } 
+ 
+}
+
+  
+void movFwd()
+{
+  digitalWrite(backOut,LOW);
+  digitalWrite(fwdOut,HIGH);
+}
+
+void stopCarts()
+{
+  digitalWrite(fwdOut,LOW);
+  digitalWrite(backOut,LOW);
+}
+
+void movRev()
+{
+  digitalWrite(fwdOut,LOW);
+  digitalWrite(backOut,HIGH); 
+}
+
+void error()
+{
+      while (1) 
+      {
+        digitalWrite(fwdOut,LOW);
+        digitalWrite(backOut,LOW);
+        digitalWrite(ERROR_PIN,HIGH);
+        Serial.println("ERROR");
+        delay(5000);
+      }
+}
+
+
