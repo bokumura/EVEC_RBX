9ecc3fd3e6e7f53e2be31b10fb632c0bcd440489
Matthew Hennes <matthew.hennes@gmail.com>
2015-03-07 14:42:06 -0800

    Revert "Added support for raising and lowering lift manually"
    This reverts commit 1fc6ab64929a59c62c6806b1422f1e135daf95a2.

--- RBX_Arduino/RBX_Arduino.ino
+++ RBX_Arduino/RBX_Arduino.ino
@@ -6,7 +6,7 @@
  Bit 4 = manual up
  Bit 5 = manual down
  Bit 6 = down endstop
- Bit 7 = unassigned
+ Bit 7 = something
  Bit 8 = unassigned
  Bit 9 = unassigned
  Bit 10 = unassigned
@@ -32,27 +32,23 @@ const int UP_IN_OFFSET = 4;
 const int DOWN_IN_OFFSET = 5;
 const int DOWN_STOP_IN = 6;
 
-//4 inputs for carts
+//4 inputs for ramp
 const int fwdIn = 16;      //B2 
 const int backIn = 14;     //B3 
 const int fwdStopIn = 4;   //D4
 const int backStopIn = 10; //B6
 
 //3 inputs for lift
-const int manualUp = 15;    //B1  
-const int manualDown = 17;  //B0
-const int downStopIn = 9;   //B5
+const int manualUp = 6;     //D7  
+const int manualDown = 12;  //D6
+const int downStopIn = 3;   //D0
 
 //ERROR PIN
 const int ERROR_PIN = 7;    //E6
 
-//2 outputs for carts
+//2 outputs
 const int fwdOut = 13;      //C7
 const int backOut = 5;      //C6
-
-//2 outputs for lift
-const int upOut = 6     //D7
-const int downOut = 12  //D6
 
 uint16_t prevState = 0x00;
 uint16_t currState = 0x00;
@@ -100,42 +96,20 @@ void loop() {
   case 0x00:
     break;
 
-  case 0x41: //MOVE FWD SWITCH IS ON
+  case 0x01: //MOVE FWD SWITCH IS ON
     movFwd();
     break;
 
-  case 0x45: //MOVE FWD, BUT FWD END IS ON
+  case 0x05: //MOVE FWD, BUT FWD END IS ON
     stopCarts();
     break;
 
-  case 0x42: //MOVE BACKWARD SWITCH IS ON
+  case 0x02: //MOVE BACKWARD SWITCH IS ON
     movRev();
     break;
 
-  case 0x4A:  //MOVE BACKWARD, BUT BACK END IS ON
+  case 0x0A:  //MOVE BACKWARD, BUT BACK END IS ON
     stopCarts();
-    break;
-    
-  case 0x14:  //MOVE UP SWITCH IS ON
-  case 0x18:
-  case 0x54:
-  case 0x58:
-    movUp();
-    break;
-    
-  case 0x04:  //CART IS RAISED, BUT NOT MOVING
-  case 0x08:
-    stopLift();
-    break;
-    
-  case 0x24:  //MOVE DOWN SWITCH IS ON
-  case 0x28:
-    movDown();
-    break;
-    
-  case 0x64:  //MOVE DOWN, BUT DOWN ENDSTOP IS ON
-  case 0x68:
-    stopLift();
     break;
 
   default: //LOL GG DONE MESSED UP SON
@@ -147,42 +121,25 @@ void loop() {
 
 
 void movFwd(){
-  digitalWrite(backOut, LOW);
-  digitalWrite(fwdOut, HIGH);
+  digitalWrite(backOut,LOW);
+  digitalWrite(fwdOut,HIGH);
 }
 
 void stopCarts(){
-  digitalWrite(fwdOut, LOW);
-  digitalWrite(backOut, LOW);
+  digitalWrite(fwdOut,LOW);
+  digitalWrite(backOut,LOW);
 }
 
 void movRev(){
-  digitalWrite(fwdOut, LOW);
-  digitalWrite(backOut, HIGH); 
-}
-
-void movUp(){
-  digitalWrite(downOut, LOW);
-  digitalWrite(upOut, HIGH);
-}
-
-void stopLift(){
-  digitalWrite(downOut, LOW);
-  digitalWrite(upOut, LOW);
-}
-
-vid movDown(){
-  digitalWrite(upOut, LOW);
-  digitalWrite(downOut, HIGH);
+  digitalWrite(fwdOut,LOW);
+  digitalWrite(backOut,HIGH); 
 }
 
 void error(){
   while (1){
-    digitalWrite(fwdOut, LOW);
-    digitalWrite(backOut, LOW);
-    digitalWrite(upOut, LOW);
-    digitalWrite(downOut, LOW);
-    digitalWrite(ERROR_PIN, HIGH);
+    digitalWrite(fwdOut,LOW);
+    digitalWrite(backOut,LOW);
+    digitalWrite(ERROR_PIN,HIGH);
     Serial.println("ERROR");
     delay(5000);
   }
